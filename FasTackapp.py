dae9ec87e00a77233d79dfef0c62073c719c562c
rachelbeal <rachelebeal2@gmail.com>
2020-06-16 23:38:08 -0700

    updating route

--- FasTackapp.py
+++ FasTackapp.py
@@ -1,22 +1,29 @@
-import plotly.express as px
 import pandas as pd
 import numpy as np
 import pygrib
 import matplotlib.pyplot as plt
 import netCDF4
-import plotly.graph_objects as go
+import math
+import plotly
+import plotly.express as px
+import plotly.figure_factory as ff
+import chart_studio.plotly as py
+import plotly.offline as py_off
+import plotly.graph_objs as go
 
-file = netCDF4.Dataset('https://nomads.ncep.noaa.gov:9090/dods/gfs_0p25_1hr/gfs20200531/gfs_0p25_1hr_00z')
+file = netCDF4.Dataset('https://nomads.ncep.noaa.gov:9090/dods/gfs_0p25_1hr/gfs20200614/gfs_0p25_1hr_00z')
 raw_lat  = np.array(file.variables['lat'][:])
 raw_lon  = np.array(file.variables['lon'][:])
 raw_wind = np.array(file.variables['gustsfc'][1,:,:])
 file.close()
 
+# set boundaries for race course
 min_lat = 0
 max_lat = 50
 min_lon = 180
 max_lon = 242
 
+# apply boundaries
 lat_to_use = np.argwhere((raw_lat >= min_lat) & (raw_lat <= max_lat))
 min_row = int(lat_to_use[0])
 max_row = int(lat_to_use[-1])
@@ -28,29 +35,50 @@ max_col = int(lon_to_use[-1])
 lat = raw_lat[lat_to_use].reshape(len(lat_to_use))
 lon = raw_lon[lon_to_use].reshape(len(lon_to_use))
 
+# filter weather data
 wind = raw_wind[min_row:max_row+1, min_col:max_col+1]
 
-BOARD_ROWS = len(wind)
-BOARD_COLS = len(wind[0])
+def racemap(routefile):
+    racemap = []
+    route = pd.read_csv(routefile)
+    for i in range(0, len(route)-1):
+        for j in range(0, len(route.values[0])-1):
+            if route.values[i][j] > 0:
+                lati = lat[i]
+                lonj = lon[j]
+                val = route.values[i][j]
+                racemap.append((lati,lonj, val))
+    racemap_df = pd.DataFrame(racemap, columns = ["lat", "lon", "val"])
+    return racemap_df
 
-board = np.zeros([BOARD_ROWS, BOARD_COLS])
+legs = ['/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0607.csv',
+       '/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0608.csv',
+       '/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0609.csv',
+       '/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0610.csv',
+       '/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0611.csv',
+       '/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0612.csv',
+       '/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0613.csv',
+       '/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0614.csv',
+       '/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0615.csv',
+       '/Users/rachelbeal/PycharmProjects/FasTack/output/Timelapse/route_lr0.5_er0.8_r10000_gamma0.95_0616.csv']
 
-#racemap = np.zeros([BOARD_ROWS, BOARD_COLS])
-visited = []
-racemap = []
+legs_df = []
 
-route_file = '/Users/rachelbeal/PycharmProjects/FasTack/route_lr0.9_er0.6_r200.csv'
-route = pd.read_csv(route_file)
+for i, filename in enumerate(legs):
+    racemap_df = racemap(filename).sort_values(by = "val").reset_index(drop=True)
+    leg = racemap_df[:15]
+    leg["leg"] = i
+    legs_df.append(leg)
 
-for i in range(0, len(route)-1):
-    for j in range(0, len(route.values[0])-1):
-        if route.values[i][j] > 0:
-            lati = lat[i]
-            lonj = lon[j]
-            val = route.values[i][j]
-            racemap.append((lati,lonj, val))
+all_legs = pd.concat(legs_df).reset_index(drop = True)
 
-racemap_df = pd.DataFrame(racemap, columns = ["lat", "lon", "val"])
+route_df = []
+
+for i, filename in enumerate(legs):
+    racemap_df = racemap(filename).sort_values(by = "val").reset_index(drop=True)
+    leg = racemap_df
+    leg["leg"] = i
+    route_df.append(leg)
 
 fig = go.Figure()
 
@@ -72,11 +100,43 @@ lon_data.append(srd.lon[i])
 lat_data.append(srd.lat[i])
 frames.append(go.Frame(data=[go.Scattergeo(lon=lon_data, lat=lat_data)]))
 
+fig = go.Figure()
+
+srd = all_legs
+
+frames = list()
+lon_data = [all_legs.lon[0]]
+lat_data = [all_legs.lat[0]]
+
+all_legs["leg"] = all_legs["leg"].astype(float)
+
+for i in range(len(all_legs.lon)):
+    if i % 5 == 0:
+        frames.append(
+            go.Frame(data=[go.Scattergeo(lon=lon_data,
+                                         lat=lat_data,
+                                         mode="markers",
+                                         marker=dict(color=all_legs["leg"][::5],
+                                                     cmin=0,
+                                                     cmax=10,
+                                                     colorscale="rainbow"
+                                                     ))
+
+                           ])
+        )
+        lon_data.append(all_legs.lon[i])
+        lat_data.append(all_legs.lat[i])
+
+lon_data.append(all_legs.lon[i])
+lat_data.append(all_legs.lat[i])
+
+frames.append(go.Frame(data=[go.Scattergeo(lon=lon_data, lat=lat_data)]))
+
 fig = go.Figure(
     data=[go.Scattergeo(
-        locationmode= 'ISO-3', mode = "lines",
-        lon=[racemap_df.lon[0], racemap_df.lon[0]],
-        lat=[racemap_df.lat[0], racemap_df.lat[0]])],
+        locationmode='ISO-3', mode="lines",
+        lon=[all_legs.lon[0], all_legs.lon[0]],
+        lat=[all_legs.lat[0], all_legs.lat[0]])],
     layout=go.Layout(
         xaxis=dict(range=[0, 5], autorange=False),
         yaxis=dict(range=[0, 5], autorange=False),
@@ -87,7 +147,20 @@ fig = go.Figure(
                           method="animate",
                           args=[None])])]
     ),
-    frames = frames
+    frames=frames
 )
+
+fig.update_layout(geo=dict(lonaxis=dict(
+    showgrid=True,
+    gridwidth=0.5,
+    range=[min_lon + 10, max_lon + 30],
+    dtick=1
+),
+    lataxis=dict(
+        showgrid=True,
+        gridwidth=0.5,
+        range=[min_lat, max_lat],
+        dtick=1
+    )))
 
 fig.show()
