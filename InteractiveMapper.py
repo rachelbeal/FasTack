d1ea1a2148e56c624bd2c33db8af7e44576b3cfb
rachelbeal <rachelebeal2@gmail.com>
2020-09-14 20:24:38 -0700

    InteractiveMapper.py

--- InteractiveMapper.py
+++ InteractiveMapper.py
@@ -8,12 +8,17 @@ import plotly
 import chart_studio.plotly as py
 import plotly.offline as py_off
 import plotly.graph_objs as go
+import plotly.figure_factory as ff
 
-file = netCDF4.Dataset('https://nomads.ncep.noaa.gov:9090/dods/gfs_0p25_1hr/gfs20200611/gfs_0p25_1hr_00z')
+print("reading gribs")
+file = netCDF4.Dataset('http://nomads.ncep.noaa.gov:80/dods/gfs_0p25/gfs20200914/gfs_0p25_00z')
 raw_lat = np.array(file.variables['lat'][:])
 raw_lon = np.array(file.variables['lon'][:])
 raw_wind = np.array(file.variables['gustsfc'][1, :, :])
+raw_wind_u = np.array(file.variables['ugrd10m'][1,:,:])
+raw_wind_v = np.array(file.variables['vgrd10m'][1,:,:])
 file.close()
+print("done reading gribs")
 
 min_lat = 0
 max_lat = 50
@@ -32,18 +37,32 @@ lat = raw_lat[lat_to_use].reshape(len(lat_to_use))
 lon = raw_lon[lon_to_use].reshape(len(lon_to_use))
 
 wind = raw_wind[min_row:max_row+1, min_col:max_col+1]
+wind_u = raw_wind_u[min_row:max_row+1, min_col:max_col+1]
+wind_v = raw_wind_v[min_row:max_row+1, min_col:max_col+1]
 
-#dead_min_row = 100
-#dead_max_row = 190
-#dead_min_col = 170
-#dead_max_col = 190
+# wind_angle = np.zeros([len(wind), len(wind[0])])
 
+# for i in range(0, len(wind_u)):
+#     for j in range (0, len(wind_v)):
+#         wind_angle[i][j] = 180 + math.degrees(math.atan2(raw_wind_u[i][j], raw_wind_v[i][j]))
 
-#dead_wind = wind
-
-#dead_wind[dead_min_row:dead_max_row, dead_min_col:dead_max_col] = 0
-
-#wind = dead_wind
+# dead_wind = wind*1
+#
+# dead_min_row = 65
+# dead_max_row = 90
+# dead_min_col = 110
+# dead_max_col = 115
+#
+# dead_wind[dead_min_row:dead_max_row, dead_min_col:dead_max_col] = 0.1
+#
+# dead_min_row = 85
+# dead_max_row = 105
+# dead_min_col = 125
+# dead_max_col = 130
+#
+# dead_wind[dead_min_row:dead_max_row, dead_min_col:dead_max_col] = 0.1
+#
+# wind = dead_wind
 
 wlat = []
 wlon = []
@@ -67,7 +86,7 @@ board = np.zeros([BOARD_ROWS, BOARD_COLS])
 visited = []
 racemap = []
 
-route_file = './output/route_lr0.7_er0.6_r500.csv'
+route_file = './output/FinalRoutes/route_lr0.5_er0.8_r10000_gamma0.95_0914.csv'
 route = pd.read_csv(route_file)
 
 for i in range(0, len(route)-1):
@@ -82,6 +101,8 @@ racemap_df = pd.DataFrame(racemap, columns = ["lat", "lon", "val"])
 
 mapbox_access_token = open("mapbox_key.txt").read()
 
+print("plotting!!")
+
 fig = go.Figure()
 
 fig.add_trace(go.Scattermapbox(
@@ -94,7 +115,8 @@ fig.add_trace(go.Scattermapbox(
         cmax = 20,
         cmin =0,
         opacity = 0.1,
-        color = weather_df.wind)))
+        color = weather_df.wind,
+        showscale = True)))
 
 
 fig.add_trace(go.Scattermapbox(
@@ -125,6 +147,11 @@ fig.update_layout(geo=dict(lonaxis=dict(
         dtick=1
     )))
 
+# x, y = np.meshgrid(lon,lat)
+#
+# f = ff.create_quiver(x, y, wind_u, wind_v)
+# f.show()
+
 # To be able to see the plot while using pycharm
 # fig.write_image('C:/Users/user/Desktop/test.png')
-# fig.show()
+fig.show()
